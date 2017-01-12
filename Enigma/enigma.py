c6cb0cee4dc138449adf385eb4b00fac2622ca87
Callum Skeet <callumskeet@gmail.com>
2017-01-12 14:40:43 +1100

    improved class implementation

--- Enigma/enigma.py
+++ Enigma/enigma.py
@@ -6,7 +6,7 @@ from rotors import *
 
 class enigma:
 
-    def settings():
+    def __init__(self):
         '''Ensures the Enigma machine has all settings
         configured for operation'''
         print('Default Configuration:\n'
@@ -14,30 +14,49 @@ class enigma:
               '\tReflector: B\n\tGround Setting: AAZ\n\tPlugboard: None')
         setting = input('Customise configuration? (Y/N): ').lower()
         if setting.startswith('y'):
-            enigma.ground_settings()
-            enigma.rotor_settings()
-            enigma.plugboard_settings()
-            return False
+            self.ground_settings()
+            self.rotor_settings()
+            self.plugboard_settings()
         else:
-            enigma.rotor_settings(True)
-            enigma.ground_settings(True)
-            enigma.plugboard_settings(True)
-            return False
+            self.rotor_settings(True)
+            self.ground_settings(True)
+            self.plugboard_settings(True)
+        return None
 
-    def ground_settings(defaults=False):
+    def main(self):
+        print(machine.encode())
+
+    # def settings():
+    #     '''Ensures the Enigma machine has all settings
+    #     configured for operation'''
+    #     print('Default Configuration:\n'
+    #           '\tModel: M3\n\tRotors (left-right): I, II, III\n'
+    #           '\tReflector: B\n\tGround Setting: AAZ\n\tPlugboard: None')
+    #     setting = input('Customise configuration? (Y/N): ').lower()
+    #     if setting.startswith('y'):
+    #         enigma.ground_settings()
+    #         enigma.rotor_settings()
+    #         enigma.plugboard_settings()
+    #     else:
+    #         enigma.rotor_settings(True)
+    #         enigma.ground_settings(True)
+    #         enigma.plugboard_settings(True)
+    #     return False
+
+    def ground_settings(self, defaults=False):
         '''Sets the ground setting (or starting position)
         of the rotors e.g. AAZ, BFX'''
         if defaults:
-            enigma.lmr = [0, 0, 25]
+            self.lmr = [0, 0, 25]
         else:
             print('Input start positions (ground setting):')
             left = ab_list.index(input('Left: ').upper())
             middle = ab_list.index(input('Middle: ').upper())
             right = ab_list.index(input('Right: ').upper())
-            enigma.lmr = [left, middle, right]
-        return enigma.lmr
+            self.lmr = [left, middle, right]
+        return self.lmr
 
-    def rotor_settings(defaults=False):
+    def rotor_settings(self, defaults=False):
         '''User selection of model, rotors and reflector'''
         models = ['M3', 'Swiss K']
         rotors = {'M3': {'I': I_roman, 'II': II_roman, 'III': III_roman,
@@ -47,10 +66,10 @@ class enigma:
                       'Swiss K': {'UKW-K': UKW_K, 'ETW-K': ETW_K}}
 
         if defaults:
-            enigma.rotors = I_roman, II_roman, III_roman, rf_b,
-            enigma.rotors += enigma.kv_swap(I_roman),
-            enigma.rotors += enigma.kv_swap(II_roman),
-            enigma.rotors += enigma.kv_swap(III_roman),
+            self.rotors = I_roman, II_roman, III_roman, rf_b,
+            self.rotors += self.kv_swap(I_roman),
+            self.rotors += self.kv_swap(II_roman),
+            self.rotors += self.kv_swap(III_roman),
 
         else:
             print('Select model: %s' % ', '.join(models))
@@ -68,23 +87,23 @@ class enigma:
             reflector = reflectors[input('Reflector: ').upper()]
 
             # Generate reverse wiring path
-            rf_l_rotor = enigma.kv_swap(l_rotor)
-            rf_m_rotor = enigma.kv_swap(m_rotor)
-            rf_r_rotor = enigma.kv_swap(r_rotor)
+            rf_l_rotor = self.kv_swap(l_rotor)
+            rf_m_rotor = self.kv_swap(m_rotor)
+            rf_r_rotor = self.kv_swap(r_rotor)
 
-            enigma.rotors = l_rotor, m_rotor, r_rotor, reflector
-            enigma.rotors += rf_l_rotor, rf_m_rotor, rf_r_rotor
+            self.rotors = l_rotor, m_rotor, r_rotor, reflector
+            self.rotors += rf_l_rotor, rf_m_rotor, rf_r_rotor
 
-        return enigma.rotors
+        return self.rotors
 
-    def plugboard_settings(defaults=False):
+    def plugboard_settings(self, defaults=False):
         '''Sets which letters are swapped on the plugboard'''
         # Generate default plugboard where A='A', B='B', etc.
         plugboard = {ltr: ltr for ltr in ab_list}
 
         if defaults:
-            enigma.plugboard = plugboard
-            return enigma.plugboard
+            self.plugboard = plugboard
+            return self.plugboard
 
         print('Input letter pairs for plugboard settings.\n'
               'Leave blank for defaults or to continue.')
@@ -97,15 +116,14 @@ class enigma:
                 while True:
                     while ltr == '' and i == 1:
                         print("Input can't be blank.")
-                        ltr = input('Enter a new letter: ').upper()
+                        ltr = input('Enter a new letter (pair %s: '
+                                    % count).upper()
                     while ltr in used_ltrs:
                         print('Letter already in use.')
                         ltr = input('Enter a new letter (pair %s): '
                                     % count).upper()
-                    if bool(ltr):
+                    if ltr or i == 0:
                         break   # second input can't be blank
-                    elif i == 0:
-                        break   # okay if first input blank
                 if ltr == '':
                     break
                 used_ltrs += ltr
@@ -115,10 +133,10 @@ class enigma:
                     plugboard[pair[1]] = pair[0]
             if pair == []:
                 break
-        enigma.plugboard = plugboard
-        return enigma.plugboard
+        self.plugboard = plugboard
+        return self.plugboard
 
-    def kv_swap(rotor):
+    def kv_swap(self, rotor):
         '''Swaps the key-value pairs in the rotor dictionary
         for use in the reverse path'''
         rf_rotor = {}
@@ -126,37 +144,35 @@ class enigma:
             rf_rotor.setdefault(v, k)
         return rf_rotor
 
-    def rotor_io(ch, pos, rotor):
+    def rotor_io(self, ch, pos, rotor):
         '''Determines the output of a letter run through a rotor'''
         rotor_mapping = ab_list.index(ch) + pos
-        if rotor_mapping > 25:
-            rotor_mapping -= 26
-        elif rotor_mapping < 0:
-            rotor_mapping += 26
+        if rotor_mapping < 0 or rotor_mapping > 25:
+            rotor_mapping %= 26
         rotor_io_out = rotor[ab_list[rotor_mapping]]
         return rotor_io_out
 
-    def plugboard_encode(keypress):
+    def plugboard_encode(self, keypress):
         '''Runs character through the plugboard'''
-        plugboard = enigma.plugboard
+        plugboard = self.plugboard
         input_letter = keypress
         if input_letter in plugboard.keys():
             output_letter = plugboard[input_letter]
         else:
             output_letter = input_letter
-        enigma.plugboard_out = output_letter
-        return enigma.plugboard_out
+        self.plugboard_out = output_letter
+        return self.plugboard_out
 
-    def rotor_encode(keypress):
+    def rotor_encode(self, keypress):
         '''Runs text through the Enigma rotor mechanism'''
-        l_rotor, m_rotor = enigma.rotors[0], enigma.rotors[1]
-        r_rotor = enigma.rotors[2]
-        reflector = enigma.rotors[3]
-        rf_l_rotor, rf_m_rotor = enigma.rotors[4], enigma.rotors[5]
-        rf_r_rotor = enigma.rotors[6]
+        l_rotor, m_rotor = self.rotors[0], self.rotors[1]
+        r_rotor = self.rotors[2]
+        reflector = self.rotors[3]
+        rf_l_rotor, rf_m_rotor = self.rotors[4], self.rotors[5]
+        rf_r_rotor = self.rotors[6]
 
         # Start position, lmr = left, middle, right
-        left, middle, right = enigma.lmr[0], enigma.lmr[1], enigma.lmr[2]
+        left, middle, right = self.lmr[0], self.lmr[1], self.lmr[2]
 
         print(ab_list[left], ab_list[middle], ab_list[right])
 
@@ -175,32 +191,32 @@ class enigma:
         if left > 25:
             left = 0
 
-        enigma.lmr[0], enigma.lmr[1], enigma.lmr[2] = left, middle, right
+        self.lmr[0], self.lmr[1], self.lmr[2] = left, middle, right
 
         # encode character
         # Subtraction compensates for the previous rotor's rotation
         # relative to the rotor being used.
-        r_rotor_out = enigma.rotor_io(keypress, right, r_rotor)
+        r_rotor_out = self.rotor_io(keypress, right, r_rotor)
 
-        m_rotor_out = enigma.rotor_io(r_rotor_out, middle - right, m_rotor)
+        m_rotor_out = self.rotor_io(r_rotor_out, middle - right, m_rotor)
 
-        l_rotor_out = enigma.rotor_io(m_rotor_out, left - middle, l_rotor)
+        l_rotor_out = self.rotor_io(m_rotor_out, left - middle, l_rotor)
 
-        reflector_out = enigma.rotor_io(l_rotor_out, -left, reflector)
+        reflector_out = self.rotor_io(l_rotor_out, -left, reflector)
 
-        l_rotor_out = enigma.rotor_io(reflector_out, left, rf_l_rotor)
+        l_rotor_out = self.rotor_io(reflector_out, left, rf_l_rotor)
 
-        m_rotor_out = enigma.rotor_io(
+        m_rotor_out = self.rotor_io(
             l_rotor_out, middle - left, rf_m_rotor)
 
-        r_rotor_out = enigma.rotor_io(
+        r_rotor_out = self.rotor_io(
             m_rotor_out, right - middle, rf_r_rotor)
 
         rotor_encode_out = ab_list[ab_list.index(r_rotor_out) - right]
-        enigma.rotor_encode_out = rotor_encode_out
-        return enigma.rotor_encode_out
+        self.rotor_encode_out = rotor_encode_out
+        return self.rotor_encode_out
 
-    def ch_input():
+    def ch_input(self):
         '''Returns only valid input characters'''
         rm_invalid_ch = re.compile(r'[A-Z]')
         keypress = ''
@@ -209,19 +225,19 @@ class enigma:
             keypress = rm_invalid_ch.findall(keypress)
         return keypress
 
-    def encode():
+    def encode(self):
         '''Runs text through the Enigma machine'''
-        keypress = enigma.ch_input()
+        keypress = self.ch_input()
         output = []
         for ch in keypress:
-            enigma.plugboard_encode(ch)
-            enigma.rotor_encode(enigma.plugboard_out)
-            enigma.plugboard_encode(enigma.rotor_encode_out)
-            output += enigma.rotor_encode_out
+            self.plugboard_encode(ch)
+            self.rotor_encode(self.plugboard_out)
+            self.plugboard_encode(self.rotor_encode_out)
+            output += self.rotor_encode_out
         output = ''.join(output)
         return output
 
 
 if __name__ == "__main__":
-    enigma.settings()
-    print(enigma.encode())
+    machine = enigma()
+    machine.main()
